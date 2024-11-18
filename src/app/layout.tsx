85d29221849c5412bb223d3505b0684d9f71f95d
kenny <kenny909@mat.umk.pl>
2024-11-18 15:51:25 +0100

    Change lang to 'pl' in html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pl">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased grid grid-rows-[auto_1fr_auto] h-screen`}
       >
