4ed3fd4f3f27af7e8175f07e3d48d3f8181132e4
kenny <kenny909@mat.umk.pl>
2024-11-18 15:49:18 +0100

    Improve footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,3 +1,26 @@
+import { FaGithub } from 'react-icons/fa';
+import Link from 'next/link';
+
 export default function Footer() {
-  return <footer className="border-foreground border-t">Footer</footer>;
+  return (
+    <footer className="bg-background text-center py-6 mt-8 shadow-lg border-t">
+      <div className="px-3 ">
+        <div className='flex justify-between p-2'>
+          {/* Links */}
+          <Link href="/opis" className="text-gray-700 hover:text-gray-900 text-sm font-medium">
+            Opis Projektu
+          </Link>
+
+          {/* GitHub Link */}
+          <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+            <FaGithub size={24} className="text-gray-700 hover:text-gray-900 transition-colors" />
+          </a>
+        </div>
+        {/* Copyright Information */}
+        <div className="text-gray-500 text-xs">
+          <p>&copy; All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
