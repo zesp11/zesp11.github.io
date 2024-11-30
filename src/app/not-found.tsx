af0e6a420571ac84bd22fc468da6bacd23687ec6
kenny <kenny909@mat.umk.pl>
2024-11-30 20:57:46 +0100

    Add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import MouseTrail from "./components/blurryMouseTrail";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full bg-gray-100 dark:bg-inherit text-gray-800 dark:text-gray-200">
+      <MouseTrail />
+      <main className="text-center z-10 p-8">
+        {/* Large Error Message */}
+        <h1 className="text-6xl font-bold mb-4">404</h1>
+        <p className="text-2xl mb-6">
+          Ups! Strona, której szukasz, nie została odnaleziona.
+        </p>
+
+        {/* Call-to-Action */}
+        <Link
+          href="/"
+          className="text-lg font-medium px-6 py-3 bg-blue-500 dark:bg-opacity-20 text-white rounded-lg hover:bg-blue-600 dark:hover:bg-blue-900 transition-colors"
+        >
+          Powrót do strony głównej
+        </Link>
+      </main>
+    </div>
+  );
+}
