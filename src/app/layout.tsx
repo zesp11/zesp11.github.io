4d818f60f0d73d6241ffa08ccc109660ad4cbc12
kenny <kenny909@mat.umk.pl>
2024-12-27 12:07:50 +0100

    Remove unused module

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./components/header";
-import Footer from "./components/footer";
 import { ThemeProvider } from "./components/theme-provider";
 
 const geistSans = localFont({
