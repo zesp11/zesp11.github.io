e79d9893f7ecbccb97c56c47e618b923ebbc38d0
Kacper Urbanski <kacperurbanski002@gmail.com>
2025-02-06 14:13:43 +0100

    Fix next.config

--- next.config.ts
+++ next.config.ts
@@ -9,8 +9,18 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+
+  /*
+  For Github static page
   basePath: '/zesp11_page',
   assetPrefix: '/zesp11_page',
+
+  For local static page
+  basePath: '/pz2024/zesp11',
+  assetPrefix: '/pz2024/zesp11',
+   */
+  basePath: '',
+  assetPrefix: '',
 };
 
 export default nextConfig;
