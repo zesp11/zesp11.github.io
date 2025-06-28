2316f3aac885090c8a2d63d555db972f6aacb8c5
Kacper Urbanski <kacperurbanski002@gmail.com>
2025-06-28 13:38:06 +0200

    Add information about deploying

--- next.config.ts
+++ next.config.ts
@@ -11,9 +11,15 @@ const nextConfig: NextConfig = {
   },
 
   /*
-  For Github static page
+  # INFO: adjust before deploy
+
+  For Github static page zesp11 organization
   basePath: '/zesp11_page',
   assetPrefix: '/zesp11_page',
+
+  For Github static page fork
+  basePath: '/zesp11.github.io',
+  assetPrefix: '/zesp11.github.io',
 
   For local static page
   basePath: '/pz2024/zesp11',
